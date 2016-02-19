56ac8f78b61a4243139d8a4cb7f079232f2d7261
OwlReporter <Niresh@Nireshs-MacBook-Pro-2.local>
2016-02-19 14:57:56 +0530

    Added Preloader
    Easy Preloader with Pure Javascript & CSS

---
+++ preload-div.js
@@ -0,0 +1,9 @@
+function writeHTMLasJS(){
+document.write("<div style=\"z-index: 100000\" id=\"owlreporter-preloader\">");
+document.write("			<div id=\"loader\"><\/div>");
+document.write("			<div class=\"loader-section section-left\"><\/div>");
+document.write("            <div class=\"loader-section section-right\"><\/div>");
+document.write("		<\/div>");
+document.write("");
+}
+writeHTMLasJS();
